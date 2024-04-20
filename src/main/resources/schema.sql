da5254b1d751b68bc042b87d444115823f68f63e
Anderson <stfn-aportador@adm.bancoamazonia.sa>
2024-04-20 16:34:24 -0300

    refactor: ajuste no nome das colunas

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,11 +1,11 @@
 CREATE TABLE IF NOT EXISTS transacao (
-                           id SERIAL PRIMARY KEY,
-                           tipo INT,
-                           data DATE,
-                           valor DECIMAL(19,2),
-                           cpf BIGINT,
-                           cartao VARCHAR(255),
-                           hora TIME,
-                           dono_da_loja VARCHAR(50),
-                           nome_da_loja VARCHAR(50)
-);
+    id SERIAL primary key,
+    tipo int,
+    data date,
+    valor decimal,
+    cpf bigint,
+    cartao varchar(255),
+    hora time,
+    dono_da_loja varchar(255),
+    nome_da_loja varchar(255)
+    );
